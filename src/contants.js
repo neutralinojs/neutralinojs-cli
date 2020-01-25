36e93086ccbdb05a09be2f240e87fc4021c80876
Shalitha Suranga <shalithasuranga@gmail.com>
2020-01-25 20:33:11 +0530

    Add javascript template

--- src/contants.js
+++ src/contants.js
@@ -3,6 +3,9 @@ module.exports = {
         ts: {
             githubUrl: 'https://github.com/neutralinojs/neutralinojs-typescript.git'
         },
+        js: {
+            githubUrl: 'https://github.com/neutralinojs/neutralinojs-javascript.git'
+        },
         angularjs: {
             githubUrl: 'https://github.com/neutralinojs/neutralinojs-angularjs-template.git'
         }
