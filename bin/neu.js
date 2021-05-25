91cf2cc6cd6264bf4b8ef1733a248586d3c11de8
Shalitha Suranga <shalithasuranga@gmail.com>
2021-05-25 15:38:16 +0530

    Remove v2 notice

--- bin/neu.js
+++ bin/neu.js
@@ -5,17 +5,6 @@ const neu = require('../src/neu-cli');
 const commons = require('../src/commons');
 const chalk = require('chalk');
 
-
-///--- v2 notice ----------
-console.log();
-console.log(chalk.red.bold.bgWhite("------- Notice --------"));
-console.log(`
-Neutralino v2 is ready for Linux/Windows. You can try v2.0.0 pre-release.
-Please notice that the macOS version is not yet supporting v2 implementaton (still in v1.5.0).
-If you are interested, you can help <3 us to implement v2-spec for macOS.
-Checkout more info: https://github.com/neutralinojs/neutralinojs
-`);
-
 neu.bootstrap(program);
 
 program.parse(process.argv);
