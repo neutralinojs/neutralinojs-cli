95d82b5f2a4303166c3a92934106b20221fe6253
Shalitha Suranga <shalithasuranga@gmail.com>
2021-05-30 16:32:00 +0530

    Bump versions

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   binaries: {
-    url: "https://github.com/neutralinojs/neutralinojs/releases/download/v2.1.0/neutralinojs-v2.1.0.zip"
+    url: "https://github.com/neutralinojs/neutralinojs/releases/download/v2.1.1/neutralinojs-v2.1.1.zip"
   },
   templates: {
     minimal: {
