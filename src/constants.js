76c1086b0a9e1de66ce9d9ff77aeeecb1915a04f
Shalitha Suranga <shalithasuranga@gmail.com>
2021-06-12 15:53:47 +0530

    Bump Neutralino version and npm package version

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   binaries: {
-    url: "https://github.com/neutralinojs/neutralinojs/releases/download/v2.2.0/neutralinojs-v2.2.0.zip"
+    url: "https://github.com/neutralinojs/neutralinojs/releases/download/v2.3.0/neutralinojs-v2.3.0.zip"
   },
   templates: {
     minimal: {
