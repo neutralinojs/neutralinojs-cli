8c93612b0955d99ddc2059925d5103f1002c4ea4
Shalitha Suranga <shalithasuranga@gmail.com>
2021-07-05 23:27:19 +0530

    Version bumps

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   remote: {
     binaries: {
-      version: "2.4.0",
+      version: "2.4.1",
       url: "https://github.com/neutralinojs/neutralinojs/releases/download/v{version}/neutralinojs-v{version}.zip"
     },
     client: {
