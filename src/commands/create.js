cd7c93759669dce5fd75f6289df50577cfb7ad8a
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-12 05:09:43 +0530

    Patched Issue [91](https://github.com/neutralinojs/neutralinojs-cli/issues/91)
    Now when passing the '.' or '../' it will inherit the binary name as the parent directory.

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,11 +1,13 @@
 const creator = require('../modules/creator');
 const utils = require('../utils');
+const path = require('path');
 
 module.exports.register = (program) => {
     program
         .command('create <binaryName>')
         .option('-t, --template [template]')
         .action(async (binaryName, command) => {
+            binaryName = path.basename(path.resolve(binaryName))
             await creator.createApp(binaryName, command.template);
             utils.figlet();
         });
