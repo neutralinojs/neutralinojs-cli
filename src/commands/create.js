fa815c8a7234fb15267842635801031b187bc1c6
ADITYA MISHRA <75035219+DEVLOPRR@users.noreply.github.com>
2022-03-12 05:43:59 +0530

    Patched Issue #91

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,12 +1,15 @@
 const creator = require('../modules/creator');
 const utils = require('../utils');
+const path = require('path');
 
 module.exports.register = (program) => {
     program
         .command('create <binaryName>')
         .option('-t, --template [template]')
         .action(async (binaryName, command) => {
-            await creator.createApp(binaryName, command.template);
+            let projectPath = path.resolve(binaryName);
+            binaryName = path.basename(path.resolve(process.cwd(), binaryName));
+            await creator.createApp(binaryName, projectPath, command.template);
             utils.figlet();
         });
 }
