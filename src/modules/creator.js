fc4824d97334689a824f424b5a96ffd9e64a34d6
pathange-s <sainatharao@gmail.com>
2022-03-22 21:17:33 +0530

    Throw error in case app name already exists in current directory

--- src/modules/creator.js
+++ src/modules/creator.js
@@ -5,6 +5,10 @@ const downloader = require('./downloader');
 const utils = require('../utils');
 
 module.exports.createApp = async (binaryName, template) => {
+    if (fs.existsSync(`./${binaryName}`)) {
+        utils.error('App name already exists');
+        process.exit(1);
+    }
     if(!template) {
         template = 'neutralinojs/neutralinojs-minimal';
     }
