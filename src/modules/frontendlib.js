bed217516782dd0c4ebc74f490562bff1b0b2057
pathange-s <sainatharao@gmail.com>
2022-04-05 18:18:51 +0530

    fix path exports and imports on windows

--- src/modules/frontendlib.js
+++ src/modules/frontendlib.js
@@ -7,14 +7,13 @@ const utils = require('../utils');
 const HOT_REL_PATCH_REGEX = constants.misc.hotReloadPatchRegex;
 let originalClientLib = null;
 
-async function makeClientLibUrl(port) {
+async function makeClientLibUrl(port) {    
     let configObj = config.get();
-    let resourcesPath = configObj.cli.resourcesPath.replace(/^\//, '');
+    let resourcesPath = utils.trimPath(configObj.cli.resourcesPath);
     let files = await recursive(resourcesPath);
-    let clientLib = files
-        .find((file) => /neutralino\.js$/.test(file))
-        .replace(/\\/g, '/'); //Fix path on windows;
-
+    let clientLib = utils.trimPath(files
+        .find((file) => /neutralino\.js$/.test(file))); //Fix path on windows;
+    console.log(clientLib);
     let url = `http://localhost:${port}`;
 
     if(clientLib) {
@@ -29,7 +28,7 @@ async function makeClientLibUrl(port) {
 
 function patchHTMLFile(clientLib) {
     let configObj = config.get();
-    let patchFile = configObj.cli.frontendLibrary.patchFile.replace(/^\//, '');
+    let patchFile = utils.trimPath(configObj.cli.frontendLibrary.patchFile);
     let html = fs.readFileSync(patchFile, 'utf8');
     let matches = HOT_REL_PATCH_REGEX.exec(html);
     if(matches) {
