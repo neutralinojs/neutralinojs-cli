7963cce925c129b963073c78abd8e4b27a383d24
William Rijksen <william.rijksen@thefuturepartners.nl>
2022-04-22 23:03:57 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -15,6 +15,7 @@ module.exports = {
     authFile: ".tmp/auth_info.json",
     binaries: {
       linux: {
+        aarch64: "neutralino-linux_aarch64",
         x64: "neutralino-linux_x64",
       },
       darwin: {
