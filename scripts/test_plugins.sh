757a72172a8067d78a2878c487dab280cb1cb427
Shalitha Suranga <shalithasuranga@gmail.com>
2022-05-02 15:37:18 +0530

    Update test_plugins.sh

--- scripts/test_plugins.sh
+++ scripts/test_plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e
 
 # Script to check if updating neu cli removes previously installed plugins
@@ -13,7 +14,7 @@ echoGreen "Uninstalling current version of neu"
 npm uninstall -g @neutralinojs/neu
 echoGreen "Installing neu 8.1.0 (older version)"
 npm install -g @neutralinojs/neu@8.1.0
-echoGreen "check neu version"
+echoGreen "Check neu version"
 neu version
 
 echoGreen "Add plugin"
@@ -27,14 +28,14 @@ npm link
 # Check if list of plugins has previously installed plugins 
 # Attempt to install plugin if it's not in CLI's node_modules
 
-echoGreen "list plugins"
+echoGreen "List plugins"
 neu plugins
 
-echoGreen "list plugins after reinstalling it"
+echoGreen "List plugins after re-installing it"
 neu plugins
 
 echoGreen "Removing plugin to revert to initial state"
 neu plugins --remove @neutralinojs/appify
 
-echoGreen "list plugins after uninstalling it"
+echoGreen "List plugins after uninstalling it"
 neu plugins
