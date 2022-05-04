86a889b6fcac541d9a3859c52fee0c6ba6f985c8
Ayman <76606666+Ayman161803@users.noreply.github.com>
2022-05-04 09:36:12 +0530

    Typo fix

--- src/commands/create.js
+++ src/commands/create.js
@@ -4,7 +4,7 @@ const utils = require('../utils');
 module.exports.register = (program) => {
     program
         .command('create <binaryName>')
-        .description('creates a app based on template (neutralinojs-minimal by default)')
+        .description('creates an app based on template (neutralinojs-minimal by default)')
         .option('-t, --template [template]')
         .action(async (binaryName, command) => {
             await creator.createApp(binaryName, command.template);
