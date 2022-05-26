452341b33b2b1e28ba9fd9d8baa587398e46c8ef
pathange-s <sainatharao@gmail.com>
2022-05-26 19:37:01 +0530

    Skeleton update of test suite with dependencies

---
+++ spec/test2.spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Tests on Strings', function () {
+  describe('string compare', function () {
+    it('should return 1 if the given two strings are equal', function () {
+      assert.equal("hmmm","hmmm");
+    });
+  });
+});
