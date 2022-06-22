2d5b72a266666536e1f0ca091dbac5be12e443e8
Shalitha Suranga <shalithasuranga@gmail.com>
2022-06-22 16:36:14 +0530

    Update runner.js

--- spec/runner.js
+++ spec/runner.js
@@ -1,24 +1,28 @@
-const { execSync } = require("child_process");
+const { execSync } = require('child_process');
 
-function run(command){
+function run(command) {
     let output = null;
     let err = null;
     let statusCode = 0;
 
-    try{
+    try {
         output = execSync(command);
     }
-    catch(error){
+    catch(error) {
         statusCode = error.status;
         err = error;
     }
-    finally{
-        return { error: decodeUTF8(err), data : decodeUTF8(output), status : statusCode};
+    finally {
+        return { 
+            error: decodeUTF8(err), 
+            data : decodeUTF8(output), 
+            status : statusCode
+        };
     }
 }
 
-function decodeUTF8(decode){
-    return (!!decode) ? decode.toString('utf8') : null;
+function decodeUTF8(decode) {
+    return decode.toString('utf8') || null;
 }
 
 module.exports = {
