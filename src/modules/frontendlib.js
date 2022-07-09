f2c61e1a08c5df22715123127652e9a023b151e4
Shalitha Suranga <shalithasuranga@gmail.com>
2022-07-09 15:20:59 +0530

    Update frontendlib.js

--- src/modules/frontendlib.js
+++ src/modules/frontendlib.js
@@ -12,7 +12,7 @@ async function makeClientLibUrl(port) {
     let resourcesPath = configObj.cli.resourcesPath.replace(/^\//, '');
     let files = await recursive(resourcesPath);
     let clientLib = files
-        ?.find((file) => /neutralino\.js$/.test(file))
+        .find((file) => /neutralino\.js$/.test(file))
         ?.replace(/\\/g, '/'); //Fix path on windows;
 
     let url = `http://localhost:${port}`;
