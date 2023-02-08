0d97d499d6cf0eafca0741a53bb41511ba658408
salihuDickson <salihudickson@gmail.com>
2023-02-08 14:52:20 +0100

    added local testing

--- src/plugins/pluginloader.js
+++ src/plugins/pluginloader.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const package = require('../../package.json');
@@ -63,6 +64,69 @@ let isPluginInstalled = (pluginName) => {
     }
 }
 
+
+module.exports.addTest = (pluginPath) => {
+    let statsObj;
+    try {
+      statsObj = fs.statSync(pluginPath);
+    } catch (e) {
+      utils.error(`${e.message}`);
+
+      process.exit(1);
+    }
+
+    if (!statsObj.isDirectory() || !fs.existsSync(pluginPath)) {
+      utils.error(`${pluginPath} is not a valid path`);
+
+      process.exit(1);
+    }
+
+    const packageJson = require(path.join(pluginPath, 'package.json'));
+    if (!packageJson) {
+      utils.error('Cannot find package.json file');
+
+      process.exit(1);
+    }
+
+    const pluginName = packageJson.name;
+    if (!pluginName) {
+      utils.error('Your plugin has no name. Please add name in package.json');
+
+      process.exit(1);
+    }
+
+    return new Promise((resolve, reject) => {
+      let plugins = [];
+      if (config.has('plugins')) plugins = config.get('plugins');
+      if (!isPluginInstalled(pluginName)) {
+        exec(`cd ${pluginPath} && npm link`, (err, stdout, stderr) => {
+          if (err) {
+            reject(stderr);
+          } else if (!plugins.includes(pluginName)) {
+            plugins.push(pluginName);
+            config.set('plugins', plugins);
+          }
+          resolve();
+        });
+
+        exec(
+          `cd ${NEU_ROOT} && npm install ${pluginPath}`,
+          (err, stdout, stderr) => {
+            if (err) {
+              reject(stderr);
+            } else if (!plugins.includes(pluginName)) {
+              plugins.push(pluginName);
+              config.set('plugins', plugins);
+            }
+            resolve();
+          }
+        );
+      } else {
+        reject(`${pluginName} is already installed!`);
+      }
+    });
+  };
+
 module.exports.remove = (pluginName, uninstallSuccessCallback) => {
     return new Promise((resolve, reject) => {
         let plugins = [];
@@ -85,6 +149,67 @@ module.exports.remove = (pluginName, uninstallSuccessCallback) => {
         }
     });
 };
+
+module.exports.removeTest = (pluginPath, uninstallSuccessCallback) => {
+    let pluginName, packageJson, statsObj;
+
+    try {
+      statsObj = fs.statSync(pluginPath);
+    } catch (e) {
+      pluginName = pluginPath;
+    }
+
+    if (!pluginName && (!statsObj?.isDirectory() || !fs.existsSync(pluginPath))) {
+      utils.error(`${pluginPath} is not a valid file path`);
+
+      process.exit(1);
+    } else {
+      packageJson = require(path.join(pluginPath, 'package.json'));
+      if (!packageJson) {
+        utils.error('Cannot find package.json file');
+
+        process.exit(1);
+      }
+
+      pluginName = packageJson.name;
+      if (!pluginName) {
+        utils.error('Your plugin has no name. Please add name in package.json');
+
+        process.exit(1);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      let plugins = [];
+      if (config.has('plugins')) plugins = config.get('plugins');
+      if (plugins.includes(pluginName)) {
+        exec(`npm rm -g ${pluginName}`, (err, stdout, stderr) => {
+          if (err) {
+            reject(stderr);
+          } else {
+            plugins.splice(plugins.indexOf(pluginName), 1);
+            config.set('plugins', plugins);
+            resolve();
+          }
+        });
+
+        exec(
+          `cd ${NEU_ROOT} && npm uninstall ${pluginName}`,
+          (err, stdout, stderr) => {
+            if (err) {
+              reject(stderr);
+            } else {
+              plugins.splice(plugins.indexOf(pluginName), 1);
+              config.set('plugins', plugins);
+              resolve();
+            }
+          }
+        );
+      } else {
+        reject(`Unable to find ${pluginName}!`);
+      }
+    });
+  };
 
 module.exports.list = () => {
     if(!config.has('plugins'))
