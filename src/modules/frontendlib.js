83cb7e2b1ebae52956b6f78d745a2550c5e26837
Kendell R <KTibow@users.noreply.github.com>
2023-10-21 09:48:32 -0700

    Update hot reload patch wording

--- src/modules/frontendlib.js
+++ src/modules/frontendlib.js
@@ -19,9 +19,9 @@ async function makeClientLibUrl(port) {
 
     let url = `http://localhost:${port}`;
 
-    if(clientLib) {
+    if (clientLib) {
         clientLib = '/' + clientLib;
-        if(configObj.documentRoot) {
+        if (configObj.documentRoot) {
             clientLib = clientLib.replace(configObj.documentRoot, '/');
         }
         url += clientLib;
@@ -38,7 +38,7 @@ function patchHTMLFile(scriptFile, regex) {
     let patchFile = configObj.cli.frontendLibrary.patchFile.replace(/^\//, '');
     let html = fs.readFileSync(patchFile, 'utf8');
     let matches = regex.exec(html);
-    if(matches) {
+    if (matches) {
         html = html.replace(regex, `$1${scriptFile}$3`);
         fs.writeFileSync(patchFile, html);
         return matches[2];
@@ -48,24 +48,37 @@ function patchHTMLFile(scriptFile, regex) {
 
 module.exports.bootstrap = async (port) => {
     let configObj = config.get();
-    if(configObj.cli.clientLibrary) {
+    if (configObj.cli.clientLibrary) {
         let clientLibUrl = await makeClientLibUrl(port);
-        originalClientLib = patchHTMLFile(clientLibUrl, HOT_REL_LIB_PATCH_REGEX);
+        originalClientLib = patchHTMLFile(
+            clientLibUrl,
+            HOT_REL_LIB_PATCH_REGEX,
+        );
     }
     let globalsUrl = await makeGlobalsUrl(port);
     originalGlobals = patchHTMLFile(globalsUrl, HOT_REL_GLOB_PATCH_REGEX);
-    utils.warn(`Hot reload patch was applied successfully. ` +
-        `Please avoid sending keyboard interrupts.`);
-    utils.log('You are working with your frontend library\'s development environment. ' +
-        'Hot reload and other tooling will work alongside with Neutralinojs API.');
-}
+    if (originalClientLib || originalGlobals) {
+        utils.warn(
+            'Neutralinojs patch was applied successfully. ' +
+                'Please avoid sending keyboard interrupts.',
+        );
+        utils.log(
+            "You are working with your frontend library's development environment. " +
+                'Your app will run in in Neutralino and be able to use the Neutralinojs APIs.',
+        );
+    } else {
+        utils.warn(
+            'Failed to apply Neutralinojs patch. Your app may not be able to use the Neutralinojs APIs.',
+        );
+    }
+};
 
 module.exports.cleanup = () => {
-    if(originalClientLib) {
+    if (originalClientLib) {
         patchHTMLFile(originalClientLib, HOT_REL_LIB_PATCH_REGEX);
     }
-    if(originalGlobals) {
+    if (originalGlobals) {
         patchHTMLFile(originalGlobals, HOT_REL_GLOB_PATCH_REGEX);
     }
-    utils.log('Hot reload patch was reverted.');
-}
+    utils.log('Neutralinojs patch was reverted.');
+};
