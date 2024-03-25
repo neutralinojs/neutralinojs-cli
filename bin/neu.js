9190818392c0148482739c6f170dc5d03a9b0518
viralgupta <guptaviral0@gmail.com>
2024-03-25 19:22:25 +0530

    webpack bundler added

--- bin/neu.js
+++ bin/neu.js
@@ -7,6 +7,8 @@ const utils = require('../src/utils');
 const program = new Command();
 neu.bootstrap(program);
 
+console.log("hello world")
+
 program.addHelpText('beforeAll', utils.getFiglet());
 program.parse(process.argv);
 
