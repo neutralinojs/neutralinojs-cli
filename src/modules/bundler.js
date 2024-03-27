757f4337c30cc316f986ab028835bbeaa9991488
viralgupta <guptaviral0@gmail.com>
2024-03-28 02:28:01 +0530

    feat: ignore files support added

--- src/modules/bundler.js
+++ src/modules/bundler.js
@@ -6,6 +6,7 @@ const config = require('./config');
 const constants = require('../constants');
 const frontendlib = require('./frontendlib');
 const utils = require('../utils');
+const { glob } = require('glob')
 
 async function createAsarFile() {
     utils.log(`Generating ${constants.files.resourceFile}...`);
@@ -13,27 +14,42 @@ async function createAsarFile() {
     const resourcesDir = utils.trimPath(configObj.cli.resourcesPath);
     const extensionsDir = utils.trimPath(configObj.cli.extensionsPath);
     const clientLibrary = configObj.cli.clientLibrary ? utils.trimPath(configObj.cli.clientLibrary)
-                            : null;
+        : null;
     const icon = utils.trimPath(configObj.modes.window.icon);
     const binaryName = configObj.cli.binaryName;
     const buildDir = configObj.cli.distributionPath ? utils.trimPath(configObj.cli.distributionPath) : 'dist';
 
     fs.mkdirSync(`.tmp`, { recursive: true });
-    await fse.copy(`./${resourcesDir}`, `.tmp/${resourcesDir}`, {overwrite: true});
-
-    if(extensionsDir && fs.existsSync(extensionsDir)) {
-        await fse.copy(`./${extensionsDir}`, `${buildDir}/${binaryName}/${extensionsDir}`, {overwrite: true});
+    let resourceFiles = await glob("**", { cwd: resourcesDir, matchBase: true, nodir: true })
+    if (configObj.cli.resourcesIgnore) {
+        const resourcesIgnoreFiles = await glob(configObj.cli.resourcesIgnore, { cwd: resourcesDir, matchBase: true, nodir: true })
+        resourceFiles = resourceFiles.filter(file => !resourcesIgnoreFiles.includes(file))
+    }
+    for (const file of resourceFiles) {
+        console.log(file)
+        await fse.copy(`./${resourcesDir}/${file}`, `.tmp/${resourcesDir}/${file}`, { overwrite: true });
     }
 
-    await fse.copy(`${constants.files.configFile}`, `.tmp/${constants.files.configFile}`, {overwrite: true});
-    if(clientLibrary) {
+    if (extensionsDir && fs.existsSync(extensionsDir)) {
+        let extensionsFiles = await glob("**", { cwd: extensionsDir, matchBase: true, nodir: true })
+        if (configObj.cli.extensionsIgnore) {
+            const extensionsIgnoreFiles = await glob(configObj.cli.extensionsIgnore, { cwd: extensionsDir, matchBase: true, nodir: true })
+            extensionsFiles = extensionsFiles.filter(file => !extensionsIgnoreFiles.includes(file))
+        }
+        for (const file of extensionsFiles) {
+            await fse.copy(`./${extensionsDir}/${file}`, `${buildDir}/${binaryName}/${extensionsDir}/${file}`, { overwrite: true });
+        }
+    }
+
+    await fse.copy(`${constants.files.configFile}`, `.tmp/${constants.files.configFile}`, { overwrite: true });
+    if (clientLibrary) {
         let typesFile = clientLibrary.replace(/.js$/, '.d.ts');
-        await fse.copy(`./${clientLibrary}`, `.tmp/${clientLibrary}`, {overwrite: true});
-        if(fs.existsSync(`.tmp/${typesFile}`)) {
+        await fse.copy(`./${clientLibrary}`, `.tmp/${clientLibrary}`, { overwrite: true });
+        if (fs.existsSync(`.tmp/${typesFile}`)) {
             fse.removeSync(`.tmp/${typesFile}`);
         }
     }
-    await fse.copy(`./${icon}`, `.tmp/${icon}`, {overwrite: true});
+    await fse.copy(`./${icon}`, `.tmp/${icon}`, { overwrite: true });
 
     await asar.createPackage('.tmp', `${buildDir}/${binaryName}/${constants.files.resourceFile}`);
 }
@@ -44,33 +60,33 @@ module.exports.bundleApp = async (isRelease, copyStorage) => {
     const buildDir = configObj.cli.distributionPath ? utils.trimPath(configObj.cli.distributionPath) : 'dist';
 
     try {
-        if(frontendlib.containsFrontendLibApp()) {
+        if (frontendlib.containsFrontendLibApp()) {
             await frontendlib.runCommand('buildCommand');
         }
 
         await createAsarFile();
         utils.log('Copying binaries...');
 
-        for(let platform in constants.files.binaries) {
-            for(let arch in constants.files.binaries[platform]) {
+        for (let platform in constants.files.binaries) {
+            for (let arch in constants.files.binaries[platform]) {
                 let originalBinaryFile = constants.files.binaries[platform][arch];
                 let destinationBinaryFile = originalBinaryFile.replace('neutralino', binaryName);
-                if(fse.existsSync(`bin/${originalBinaryFile}`)) {
+                if (fse.existsSync(`bin/${originalBinaryFile}`)) {
                     fse.copySync(`bin/${originalBinaryFile}`, `${buildDir}/${binaryName}/${destinationBinaryFile}`);
                 }
             }
         }
 
-        for(let dependency of constants.files.dependencies) {
-            fse.copySync(`bin/${dependency}`,`${buildDir}/${binaryName}/${dependency}`);
+        for (let dependency of constants.files.dependencies) {
+            fse.copySync(`bin/${dependency}`, `${buildDir}/${binaryName}/${dependency}`);
         }
 
-        if(copyStorage) {
+        if (copyStorage) {
             utils.log('Copying storage data...');
             try {
-                fse.copySync('.storage',`${buildDir}/${binaryName}/.storage`);
+                fse.copySync('.storage', `${buildDir}/${binaryName}/.storage`);
             }
-            catch(err) {
+            catch (err) {
                 utils.error('Unable to copy storage data from the .storage directory. Please check if the directory exists');
                 process.exit(1);
             }
