4bcdbb5d324bba1d3e393e806beceec44cb64293
viralgupta <guptaviral0@gmail.com>
2024-04-11 06:02:34 +0530

    chore: removed console.log

--- bin/neu.js
+++ bin/neu.js
@@ -7,8 +7,6 @@ const utils = require('../src/utils');
 const program = new Command();
 neu.bootstrap(program);
 
-console.log("hello world")
-
 program.addHelpText('beforeAll', utils.getFiglet());
 program.parse(process.argv);
 
