3c5773fb77612d93c40b7a9f865ff2fe885969b2
Sadaf-A <sadaf20043006@gmail.com>
2024-04-18 13:07:25 +0530

    added option to clear the current build

--- src/commands/build.js
+++ src/commands/build.js
@@ -8,12 +8,15 @@ module.exports.register = (program) => {
         .description('builds binaries for all supported platforms and resources.neu file')
         .option('-r, --release')
         .option('--copy-storage')
+        .option('--clear-build')
         .action(async (command) => {
             utils.checkCurrentProject();
             const configObj = config.get()
-            utils.log('Removing current build...');
             const buildDir = configObj.cli.distributionPath ? utils.trimPath(configObj.cli.distributionPath) : 'dist';
-            utils.clearDirectory(buildDir);
+            if(command.clearBuild) {
+                utils.log('Removing current build...');
+                utils.clearDirectory(buildDir);
+            }
             utils.log('Bundling app...');
             await bundler.bundleApp(command.release, command.copyStorage);
             utils.showArt();
