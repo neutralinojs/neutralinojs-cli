e3628e0cca3d95d9091a6716e899c240ad7a1d76
Sadaf-A <sadaf20043006@gmail.com>
2024-04-22 17:16:27 +0530

    modified: utils.js to print ASCII art from console.log() directly

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 const process = require('process');
-const figlet = require('figlet');
 const chalk = require('chalk');
 const constants = require('./constants');
 const CONFIG_FILE = constants.files.configFile;
@@ -16,7 +15,15 @@ let isNeutralinojsProject = () => {
 }
 
 let getFiglet = () => {
-    return figlet.textSync('Neutralinojs');
+    const neutralinoText = 
+ `  _   _            _             _ _             _
+ | \\ | | ___ _   _| |_ _ __ __ _| (_)_ __   ___ (_)___
+ |  \\| |/ _ \\ | | | __| '__/ _' | | | '_ \\ / _ \\| / __|
+ | |\\  |  __/ |_| | |_| | | (_| | | | | | | (_) | \\__ \\
+ |_| \\_|\\___|\\__,_|\\__|_|  \\__,_|_|_|_| |_|\\___// |___/
+                                               |__/`
+
+return neutralinoText;;
 }
 
 let showArt = () => {
