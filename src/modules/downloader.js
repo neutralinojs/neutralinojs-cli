df3cbbffe0abf7d231f039c4cf5d15990025ce10
Cosmo Myzrail Gorynych <admin@nersta.ru>
2024-12-18 22:21:30 +1200

    :zap: Escape { } symbols in regEx statements This is not needed syntax-wise but it drove syntax highlighting in VSCode mad, as {} are usually used for quantifiers in regEx.

--- src/modules/downloader.js
+++ src/modules/downloader.js
@@ -66,7 +66,7 @@ let getBinaryDownloadUrl = async (latest) => {
         config.update('cli.binaryVersion', version);
     }
     return constants.remote.binariesUrl
-        .replace(/{tag}/g, utils.getVersionTag(version));
+        .replace(/\{tag\}/g, utils.getVersionTag(version));
 }
 
 let getClientDownloadUrl = async (latest, types = false) => {
@@ -86,7 +86,7 @@ let getClientDownloadUrl = async (latest, types = false) => {
 
     let scriptUrl = constants.remote.clientUrlPrefix + (types ? 'd.ts' : getScriptExtension());
     return scriptUrl
-        .replace(/{tag}/g, utils.getVersionTag(version));
+        .replace(/\{tag\}/g, utils.getVersionTag(version));
 }
 
 let getTypesDownloadUrl = (latest) => {
