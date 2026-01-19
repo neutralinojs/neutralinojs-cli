0babeb704ebe3d6587134affe140c5a9bcbeb2a0
subhu339 <sk3656770@gmail.com>
2026-01-19 15:30:04 +0530

    fix: use compatible syntax and implement dev server cleanup

--- src/commands/build.js
+++ src/commands/build.js
@@ -15,7 +15,6 @@ module.exports.register = (program) => {
         .option('--clean')
         .option('--macos-bundle')
         .action(async (command) => {
-            // Use custom config file if provided
             if (command.configFile) {
                 utils.log(`Using config file: ${command.configFile}`);
                 constants.files.configFile = command.configFile;
@@ -23,26 +22,10 @@ module.exports.register = (program) => {
 
             utils.checkCurrentProject();
 
-            if (frontendlib.containsFrontendLibApp()) {
-    utils.log('Running frontend build...');
-    const proc = frontendlib.runCommand('buildCommand');
-
-    if (proc) {
-        await new Promise((resolve, reject) => {
-            proc.on('exit', (code) => code === 0 ? resolve() : reject(new Error(`Frontend build exited with code ${code}`)));
-            proc.on('error', reject);
-        });
-        utils.log('Frontend build finished.');
-    }
-
-    // Prevent bundler from running frontend build
-    frontendlib.runCommand = () => null;
-}
-
-
-            // Get distribution path
             const configObj = config.get();
-            const buildDir = configObj.cli.distributionPath ? utils.trimPath(configObj.cli.distributionPath) : 'dist';
+            const buildDir = configObj.cli.distributionPath
+                ? utils.trimPath(configObj.cli.distributionPath)
+                : 'dist';
 
             if (command.clean) {
                 utils.log(`Cleaning previous build files from ${buildDir}...`);
@@ -61,4 +44,4 @@ module.exports.register = (program) => {
             utils.log(`Application package was generated at the ${buildDir} directory!`);
             utils.log('Distribution guide: https://neutralino.js.org/docs/distribution/overview');
         });
-}
+};
