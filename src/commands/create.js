61c253ecafa7f7ba2bdfdd365599b8777fdc5756
JATIN <Jatin04072005@gmail.com>
2026-01-19 17:08:00 +0530

    Improving Error hitting on neu create without binaryName

--- src/commands/create.js
+++ src/commands/create.js
@@ -2,13 +2,19 @@ const creator = require('../modules/creator');
 const utils = require('../utils');
 
 module.exports.register = (program) => {
-    program
-        .command('create <binaryName>')
-        .description('creates an app based on template (neutralinojs/neutralinojs-minimal by default)')
-        .option('-t, --template [template]')
-        .action(async (binaryName, command) => {
-            await creator.createApp(binaryName, command.template);
-            utils.showArt();
-        });
+   program
+  .command('create <binaryName>')
+  .description('creates an app based on template (neutralinojs/neutralinojs-minimal by default)')
+  .option('-t, --template [template]')
+  .addHelpText('after', `
+Examples:
+  neu create my_app
+  neu create my_app --template neutralinojs/neutralinojs-minimal
+`)
+  .action(async (binaryName, command) => {
+      await creator.createApp(binaryName, command.template);
+      utils.showArt();
+  });
+
 }
 
