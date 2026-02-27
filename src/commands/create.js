ed984d96335fa625f37a9066095e2721a18f19c6
abhi26-21 <your-email@example.com>
2026-02-27 21:17:56 +0530

    feat(create): add examples section to help output

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,14 +1,13 @@
-const creator = require('../modules/creator');
-const utils = require('../utils');
-
-module.exports.register = (program) => {
-    program
-        .command('create <binaryName>')
-        .description('creates an app based on template (neutralinojs/neutralinojs-minimal by default)')
-        .option('-t, --template [template]')
-        .action(async (binaryName, command) => {
-            await creator.createApp(binaryName, command.template);
-            utils.showArt();
-        });
-}
-
+program
+  .command('create <binaryName>')
+  .description('creates an app based on template (neutralinojs/neutralinojs-...)')
+  .option('-t, --template [template]')
+  .addHelpText('after', `
+Examples:
+  $ neu create myapp
+  $ neu create myapp --template neutralinojs/neutralinojs-zero
+`)
+  .action(async (binaryName, command) => {
+    await creator.createApp(binaryName, command.template);
+    utils.showArt();
+  });
